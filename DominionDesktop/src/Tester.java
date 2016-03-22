ab32558b111cc8b9b7c0010a5fef6e51298d6fc6
jorrevynckier <jorre.vynckier@student.howest.be>
2016-03-22 18:03:57 +0100

    testing a small change to desktop version

--- DominionDesktop/src/Tester.java
+++ DominionDesktop/src/Tester.java
@@ -1,6 +1,6 @@
 
 public class Tester {
 	public static void main(String[] args) {
-		System.out.println("hello again desktop now modified");
+		System.out.println("hello again desktop now modif ied");
 	}
 }
