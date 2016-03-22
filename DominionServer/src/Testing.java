d4dc35f319b29559d286e91ac1a0897a2ea25289
jensvangeel <jensvangeel@outlook.com>
2016-03-22 13:00:17 +0100

    aanpassing

--- DominionServer/src/Testing.java
+++ DominionServer/src/Testing.java
@@ -1,12 +1,11 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+//dit is een aanpassing door jens
 
 
 @WebServlet("/Jorre")
